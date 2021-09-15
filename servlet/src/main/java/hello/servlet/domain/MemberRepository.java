8ea769dcbd10e858a7f8f02866723a0e8309a8a0
HyeokChan <qpjiq123@gmail.com>
2021-09-16 00:50:31 +0900

    memberRepository 간단한 기능 추가, 테스트

--- servlet/src/main/java/hello/servlet/domain/MemberRepository.java
+++ servlet/src/main/java/hello/servlet/domain/MemberRepository.java
@@ -32,4 +32,9 @@ public class MemberRepository {
     public List<Member> findAll(){
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore(){
+        store.clear();
+    }
+
 }
