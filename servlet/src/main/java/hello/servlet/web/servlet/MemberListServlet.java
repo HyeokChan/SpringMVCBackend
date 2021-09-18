f372ac96f0dd4762a76612be25354d6254edb73f
HyeokChan <qpjiq123@gmail.com>
2021-09-19 00:06:50 +0900

    welcome page 변경

--- servlet/src/main/java/hello/servlet/web/servlet/MemberListServlet.java
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -38,13 +38,6 @@ public class MemberListServlet extends HttpServlet {
         w.write(" <th>age</th>");
         w.write(" </thead>");
         w.write(" <tbody>");
-        /*
-        w.write(" <tr>");
-        w.write(" <td>1</td>");
-        w.write(" <td>userA</td>");
-        w.write(" <td>10</td>");
-        w.write(" </tr>");
-        */
         for (Member member : members) {
             w.write(" <tr>");
             w.write(" <td>" + member.getId() + "</td>");
